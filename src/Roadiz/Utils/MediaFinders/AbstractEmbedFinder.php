e10ff2214da872a92107a13244c1f8593ce25dff
Ambroise Maupate <ambroise@rezo-zero.com>
2018-09-03 19:18:17 +0200

    Fixed roadiz/roadiz#317 - add allow attribute on iframes.

--- src/Roadiz/Utils/MediaFinders/AbstractEmbedFinder.php
+++ src/Roadiz/Utils/MediaFinders/AbstractEmbedFinder.php
@@ -191,6 +191,7 @@ abstract class AbstractEmbedFinder
          * getSource method will resolve all options for us.
          */
         $attributes['src'] = $this->getSource($options);
+        $attributes['allow'] = [];
 
         if ($options['width'] > 0) {
             $attributes['width'] = $options['width'];
@@ -212,14 +213,22 @@ abstract class AbstractEmbedFinder
         $attributes['class'] = $options['class'];
         $attributes['frameborder'] = "0";
 
+        if ($options['autoplay']) {
+            $attributes['allow'][] = 'autoplay';
+        }
+
         if ($options['fullscreen']) {
             $attributes['webkitAllowFullScreen'] = "1";
             $attributes['mozallowfullscreen'] = "1";
             $attributes['allowFullScreen'] = "1";
+            $attributes['allow'][] = 'fullscreen';
+        }
+
+        if (count($attributes['allow']) > 0) {
+            $attributes['allow'] = implode('; ', $attributes['allow']);
         }
 
         $attributes = array_filter($attributes);
-
 
         $htmlAttrs = [];
         foreach ($attributes as $key => $value) {
