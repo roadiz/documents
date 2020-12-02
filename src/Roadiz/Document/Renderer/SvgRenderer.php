aa586b67dcb3d19da249815395dff1c8e12d0092
Ambroise Maupate <ambroise@rezo-zero.com>
2020-12-02 22:33:34 +0100

    Switch SVG tag from object to img

--- src/Roadiz/Document/Renderer/SvgRenderer.php
+++ src/Roadiz/Document/Renderer/SvgRenderer.php
@@ -40,9 +40,11 @@ class SvgRenderer implements RendererInterface
     {
         $options = $this->viewOptionsResolver->resolve($options);
         $assignation = array_filter($options);
-
         $attributes = $this->getAttributes($assignation);
-        $attributes['data'] = $this->packages->getUrl($document->getRelativePath() ?? '', Packages::DOCUMENTS);
+        $attributes['src'] = $this->packages->getUrl(
+            $document->getRelativePath() ?? '',
+            Packages::DOCUMENTS
+        );
 
         $attrs = [];
         foreach ($attributes as $key => $value) {
@@ -52,7 +54,7 @@ class SvgRenderer implements RendererInterface
             $attrs[] = $key . '="' . $value . '"';
         }
 
-        return '<object ' . implode(' ', $attrs) . '></object>';
+        return '<img ' . implode(' ', $attrs) . ' />';
     }
 
     /**
@@ -62,11 +64,16 @@ class SvgRenderer implements RendererInterface
      */
     protected function getAttributes(array $options): array
     {
-        $attributes = [
-            'type' => 'image/svg+xml'
-        ];
+        $attributes = [];
+        $allowedAttributes = array_merge(
+            SvgDocumentViewer::$allowedAttributes,
+            [
+                'loading',
+                'alt'
+            ]
+        );
         foreach ($options as $key => $value) {
-            if (in_array($key, SvgDocumentViewer::$allowedAttributes)) {
+            if (in_array($key, $allowedAttributes)) {
                 if ($key === 'identifier') {
                     $attributes['id'] = $value;
                 } else {
