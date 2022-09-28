7f929c96d2d97353b191a0a26ab27c1a8cd14f74
Ambroise Maupate <ambroise@rezo-zero.com>
2022-09-28 17:08:22 +0200

    feat(Events): Deprecated all documents upload events which are dispatched before DB flush.

--- src/Roadiz/Core/Events/DocumentFileUploadedEvent.php
+++ src/Roadiz/Core/Events/DocumentFileUploadedEvent.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace RZ\Roadiz\Core\Events;
 
+/**
+ * @deprecated This event is dispatch before Document is flushed into DB thus document Identifier is not known to Messenger dispatchers. Use DocumentFileUpdatedEvent instead.
+ */
 final class DocumentFileUploadedEvent extends FilterDocumentEvent
 {
 }
