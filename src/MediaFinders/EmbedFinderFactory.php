ebe69078fdec15de9dfb7587b4dcfb9a6468d0ff
Ambroise Maupate <ambroise@rezo-zero.com>
2023-01-05 15:00:36 +0100

    feat: Added createForUrl method in EmbedFinderFactory

--- src/MediaFinders/EmbedFinderFactory.php
+++ src/MediaFinders/EmbedFinderFactory.php
@@ -41,6 +41,52 @@ class EmbedFinderFactory
         return null;
     }
 
+    public function createForUrl(?string $embedUrl): ?EmbedFinderInterface
+    {
+        if (null === $embedUrl) {
+            throw new \InvalidArgumentException('"embedUrl" is required');
+        }
+        // Throws a BadRequestHttpException if the embedUrl is not a string
+        if (!is_string($embedUrl)) {
+            throw new \InvalidArgumentException('"embedUrl" must be a string');
+        }
+        // Throws a BadRequestHttpException if the embedUrl is not a valid URL
+        if (!filter_var($embedUrl, FILTER_VALIDATE_URL)) {
+            throw new \InvalidArgumentException('"embedUrl" is not a valid URL');
+        }
+
+        // get embedFinder from embedUrl according to starting URL pattern
+        switch (true) {
+            case str_starts_with($embedUrl, 'https://www.youtube.com/'):
+            case str_starts_with($embedUrl, 'https://youtube.com/'):
+            case str_starts_with($embedUrl, 'https://youtu.be/'):
+                return $this->createForPlatform('youtube', $embedUrl);
+            case str_starts_with($embedUrl, 'https://vimeo.com/'):
+            case str_starts_with($embedUrl, 'https://www.vimeo.com/'):
+                return $this->createForPlatform('vimeo', $embedUrl);
+            case str_starts_with($embedUrl, 'https://open.spotify.com'):
+                return $this->createForPlatform('spotify', $embedUrl);
+            case str_starts_with($embedUrl, 'https://dailymotion.com'):
+            case str_starts_with($embedUrl, 'https://www.dailymotion.com'):
+                return $this->createForPlatform('dailymotion', $embedUrl);
+            case str_starts_with($embedUrl, 'https://twitch.tv'):
+            case str_starts_with($embedUrl, 'https://www.twitch.tv'):
+                return $this->createForPlatform('twitch', $embedUrl);
+            case str_starts_with($embedUrl, 'https://www.ted.com/talks'):
+                return $this->createForPlatform('ted', $embedUrl);
+            case str_starts_with($embedUrl, 'https://www.mixcloud.com'):
+                return $this->createForPlatform('mixcloud', $embedUrl);
+            case str_starts_with($embedUrl, 'https://www.deezer.com'):
+                return $this->createForPlatform('deezer', $embedUrl);
+            case str_starts_with($embedUrl, 'https://api.soundcloud.com'):
+            case str_starts_with($embedUrl, 'https://www.soundcloud.com'):
+            case str_starts_with($embedUrl, 'https://soundcloud.com'):
+                return $this->createForPlatform('soundcloud', $embedUrl);
+        }
+
+        return null;
+    }
+
     /**
      * @param string|null $mediaPlatform
      *
