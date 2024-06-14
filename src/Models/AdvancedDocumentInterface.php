2d10223f1eef5bfe363628f3e51bffcb2b732955
roadiz-ci <ambroise+ci@roadiz.io>
2024-06-14 10:47:35 +0000

    chore: Bumped

--- src/Models/AdvancedDocumentInterface.php
+++ src/Models/AdvancedDocumentInterface.php
@@ -4,8 +4,19 @@ declare(strict_types=1);
 
 namespace RZ\Roadiz\Documents\Models;
 
-interface AdvancedDocumentInterface extends DocumentInterface, SizeableInterface, DisplayableInterface
+interface AdvancedDocumentInterface extends DocumentInterface, SizeableInterface
 {
+    /**
+     * @return string|null
+     */
+    public function getImageAverageColor(): ?string;
+
+    /**
+     * @param string|null $imageAverageColor
+     * @return $this
+     */
+    public function setImageAverageColor(?string $imageAverageColor): static;
+
     /**
      * @return int|null
      */
