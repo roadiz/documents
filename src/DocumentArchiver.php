1e9501ddc8542a58a49edd57d3b20b4b1994a473
roadiz-ci <ambroise+ci@roadiz.io>
2024-09-24 13:09:45 +0000

    Merge branch hotfix/v2.3.28

--- src/DocumentArchiver.php
+++ src/DocumentArchiver.php
@@ -16,21 +16,18 @@ use Symfony\Component\String\Slugger\AsciiSlugger;
  */
 final class DocumentArchiver
 {
-    private FilesystemOperator $documentsStorage;
-
-    public function __construct(FilesystemOperator $documentsStorage)
+    public function __construct(private readonly FilesystemOperator $documentsStorage)
     {
-        $this->documentsStorage = $documentsStorage;
     }
 
     /**
-     * @param array $documents
+     * @param iterable<DocumentInterface> $documents
      * @param string $name
      * @param bool $keepFolders
      * @return string Zip file path
      * @throws FilesystemException
      */
-    public function archive(array $documents, string $name, bool $keepFolders = true): string
+    public function archive(iterable $documents, string $name, bool $keepFolders = true): string
     {
         $filename = (new AsciiSlugger())->slug($name . ' ' . date('YmdHis'), '_') . '.zip';
         $tmpFileName = sys_get_temp_dir() . DIRECTORY_SEPARATOR . $filename;
@@ -60,8 +57,16 @@ final class DocumentArchiver
         return $tmpFileName;
     }
 
+    /**
+     * @param iterable<DocumentInterface> $documents
+     * @param string $name
+     * @param bool $keepFolders
+     * @param bool $unlink
+     * @return BinaryFileResponse
+     * @throws FilesystemException
+     */
     public function archiveAndServe(
-        array $documents,
+        iterable $documents,
         string $name,
         bool $keepFolders = true,
         bool $unlink = true
